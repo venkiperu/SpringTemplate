acadfe0f97fe9df0ddf76b3f18f9c16939b7674d
Gabriel Novoa <gnovoab@gmail.com>
2014-04-08 11:48:18 +0100

    Refactoring

--- src/main/java/com/gnovoa/controller/HelloController.java
+++ src/main/java/com/gnovoa/controller/HelloController.java
@@ -7,15 +7,24 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
+import java.util.Properties;
+
 @Controller
-@RequestMapping("/")
 public class HelloController
 {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
-	@RequestMapping(method = RequestMethod.GET)
+	@Resource (name = "systemProperties")
+	Properties systemProperties;
+
+	@Resource (name = "allProperties")
+	Properties properties;
+
+	@RequestMapping(value="/", method= RequestMethod.GET)
 	public String printWelcome(ModelMap model)
     {
+
         log.info("this is a info msg");
         log.debug("this is a debug msg");
         log.warn("this is a warn msg");
